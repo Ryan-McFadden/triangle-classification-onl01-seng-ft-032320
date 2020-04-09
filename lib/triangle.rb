30ca3a90b93de8fb92b7e930e162f6e4260e5862
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-09 03:14:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
   def kind
     if a <= 0 || b <= 0 || c <= 0 
       raise TriangleError
-    elsif @a + @b <= @c || @a + @c <= @b || @b + @C <= @a
+    elsif a + b <= c || a + c <= b || b + C <= a
       raise TriangleError
     elsif
       if a == b && b == c && a == c
